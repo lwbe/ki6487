2b73cc23298e68a0683c35fa50f2c385e5c30ef1
Lorenzo Bernardi <lorenzo.bernardi@llr.in2p3.fr>
2024-04-02 17:31:22 +0200

    ajoute le module usbserial et confstring

--- get_new_usb_devices.py
+++ get_new_usb_devices.py
@@ -1,7 +1,14 @@
 import serial.tools.list_ports
 
 
+def get_current_devices_list():
+    return [p for p in serial.tools.list_ports.comports()]
+    
 def get_info_from_device_path(device_paths, devices):
+    """
+    extract data form serial.tools.list_ports for certain device path (ex /sys/devices/pci0000:00/0000:00:14.0/usb1/1-10/1-10:1.0/ttyUSB0
+)
+    """
     r=[]
     for i in device_paths:
         for d in devices:
@@ -12,32 +19,71 @@ def get_info_from_device_path(device_paths, devices):
                           "sn":d.serial_number})
     return r
 
-previous_devices = [p for p in serial.tools.list_ports.comports()]
-previous_dev_path = [p.device_path for p in previous_devices]
+def get_dev_path_from_pid_vid_sn(pid,vid,sn,devices):
 
-input('press return when new device is inserted or removed')
-new_devices = [p for p in serial.tools.list_ports.comports()]
-new_dev_path = [p.device_path for p in new_devices]
-
-tmp = previous_dev_path[:]
-
-for i in previous_dev_path:
-    if i in new_dev_path:
-        new_dev_path.remove(i)
-        tmp.remove(i)
-
-if tmp:
-    print("Removed device ")
-    
-    for d in get_info_from_device_path(tmp, previous_devices):
-        print("--")
+    for d in devices:
+        if pid:
+            if pid != d.pid:
+                continue
+        if vid:
+            if vid != d.vid:
+                continue
+        if sn:
+            if sn != d.sn:
+                continue
+        return d.device
+    return None
+            
+def print_info(device_data):
+    for i,d in enumerate(device_data):
+        print("device %d" % i)
         for k, v in d.items():
             print(f"\t{k} : {v}")
+                
 
 
-if new_dev_path:
-    print("Added device ")
-    for d in get_info_from_device_path(new_dev_path, new_devices):
-        print("--")
-        for k, v in d.items():
-            print(f"\t{k} : {v}")
+if __name__ == "__main__":
+
+    # get a list of device and extract the device path which is unique
+    previous_devices = get_current_devices_list()
+    previous_dev_path = [p.device_path for p in previous_devices]
+
+    input('press return when new device is inserted or removed')
+
+    # get a list of device after plug in or out a device and extract the device path which is unique
+    new_devices = get_current_devices_list()
+    new_dev_path = [p.device_path for p in new_devices]
+
+
+    # compare the two lists and extract the difference
+    tmp = previous_dev_path[:]
+
+    for i in previous_dev_path:
+        if i in new_dev_path:
+            new_dev_path.remove(i)
+            tmp.remove(i)
+
+    # print if one device has been removed
+    if tmp:
+        print("REMOVED device ")
+        data = get_info_from_device_path(tmp, previous_devices)
+        print_info(data)
+        if data:
+            for d in data:
+                found_path = get_dev_path_from_pid_vid_sn(d["pid"],d["vid"],d["sn"],previous_devices)
+                print(d,found_path)
+    # or 
+    if new_dev_path:
+        print("ADDED device ")
+        data = get_info_from_device_path(new_dev_path, new_devices)
+        print_info(data)
+        if data:
+            for d in data:
+                found_path = get_dev_path_from_pid_vid_sn(d["pid"],d["vid"],d["sn"],new_devices)
+                print(d,found_path)
+
+
+
+    print(get_dev_path_from_pid_vid_sn(8200,1367,"",previous_devices))
+    print(get_dev_path_from_pid_vid_sn(8200,1367,"",new_devices))
+
--- ki6487.py
+++ ki6487.py
@@ -1,4 +1,75 @@
+from get_new_usb_devices import get_current_devices_list, get_dev_path_from_pid_vid_sn
 import serial
+import re
 
 
 
+class ConfStringL(object):
+    """
+    lighweight confstring typically pid=xxxx,vid=yyyyy,sn=zzzzz,baudrate=cccc,....
+    """
+    def __init__(self, confstring,confstring_def={}):
+        
+        self.conf_as_dict={}
+        for k,v in [i.split("=",maxsplit=1) for i in confstring.split(",")]:
+            if confstring_def.get(k):                
+                v = confstring_def.get(k)(v)
+                    
+            self.conf_as_dict.update(((k,v),))
+        print(self.conf_as_dict)
+
+    def get_conf(self):
+        return self.conf_as_dict
+
+
+
+class UsbSerial(object):
+    confstring_def={
+        "pid":int,
+        "vid":int,
+        "baudrate":int,
+        "timeout":int,
+        }
+    def __init__(self, confstring_l):
+        # analyse the confstring
+        conf = ConfStringL(confstring_l, self.confstring_def).get_conf()
+        pid = conf.pop("pid")
+        vid = conf.pop("vid")
+        sn = conf.pop("sn")
+        if sn == "None":
+            sn = None
+
+        device_path = get_dev_path_from_pid_vid_sn(pid,vid,sn,get_current_devices_list())
+        print(f"device path {device_path}")
+
+        # configure the device
+        self.device = serial.Serial(device_path,**conf)
+
+
+    def _send(self,string):
+        pass
+
+    def __receive(self):
+        return "OK"
+
+
+
+
+class ki6487(object):
+    def __init__(self, confstring):
+
+        comm_device_type, comm_confstring = re.search("([^(]*)\((.*)\)",confstring).groups()
+        
+        if comm_device_type == "usbserial":
+            self.comm_device = UsbSerial(comm_confstring)
+        else:
+            raise Exception("commnunication protocol {comm_device_type} not implemented")
+        
+
+
+
+        
+        
+if __name__ == "__main__":
+    
+    k = ki6487("usbserial(pid=8200,vid=1367,sn=None,baudrate=57600,timeout=1)")
